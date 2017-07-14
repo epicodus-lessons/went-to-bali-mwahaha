a02b0fdff8f28e750733165b443eb61d0638a957
akane0915 <asialkane@gmail.com>
2017-07-14 09:15:41 -0700

    add ajax to add item to shopping cart on index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Product.destroy_all
 
   def generate_products
     20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop, image: Faker::Avatar.image)
     end
   end
 end
