004306e54cac05e0ce0ecf89c352716456072c50
nkls <niklas@long.ch>
2017-07-14 11:15:57 -0700

    add image to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,5 @@
 class Product < ApplicationRecord
+  has_attached_file :image
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   has_many :order_items
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@ class Seed
   end
 
   def generate_products
-    20.times do |i|
-      Product.create!(name: Faker::Lorem.word, price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
+    Product.destroy_all
+    10.times do |i|
+      Product.create!(name: Faker::Lorem.word, image: File.open("#{Rails.root}/public/seed_images/#{i}.jpg"),price: rand(10...100), description: Faker::Lorem.sentence(5, false, 0).chop)
     end
   end
 end
