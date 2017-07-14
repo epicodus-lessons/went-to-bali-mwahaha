f448311939c3f610be4abbe7e764d19bbdcfef66
akane0915 <asialkane@gmail.com>
2017-07-14 14:41:39 -0700

    add admin link in navbar to route to new product form

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,6 +29,6 @@ class ProductsController < ApplicationController
 
   private
   def product_params
-    params.require(:product).permit(:name, :description, :price)
+    params.require(:product).permit(:name, :description, :price, :image)
   end
 end
