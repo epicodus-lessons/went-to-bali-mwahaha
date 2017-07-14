ac954190476677494bae352a6bd8ff0e74829c97
nkls <niklas@long.ch>
2017-07-14 12:25:33 -0700

    add product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,12 @@
 class Product < ApplicationRecord
   has_attached_file :image
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
+  
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image, presence: true
+  
   has_many :order_items, dependent: :destroy
 end
